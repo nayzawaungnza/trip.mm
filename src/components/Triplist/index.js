f46efc3480f7466439dd87aec96297f78f90b559
nayzawaung750nza <nayzawaung750@gmail.com>
2024-02-28 15:40:02 +0630

    Ep-34-Custom-hook

--- src/components/Triplist/index.js
+++ src/components/Triplist/index.js
@@ -1,35 +1,38 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import useFetch from '../../hooks/useFetch';
+import './index.css'
 
 export default function Triplist() {
-    let [trips, setTrips] = useState([]);
-    let [url, setUrl] = useState('http://localhost:3001/trips');
-    let fetchTrips = useCallback(() =>{
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>{
-        setTrips(data);
-        })
-    },[url])
+    let [url, setUrl] = useState('http://localhost:3001/tripsff');
+    let {data : trips, loading, error} = useFetch(url)
+        
+    
 
-    useEffect(()=>{
-        fetchTrips()
-    }, [fetchTrips])
-    console.log(trips);
+    
   return (
-    <div>
-        <h1>Ready to go?</h1>
-        <button onClick={()=>setUrl('http://localhost:3001/trips')}>All</button>
-        <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>Trip in Myanmar</button>
-    <ul>
-        {
-            trips.map(trip=>(
-                <li key={trip.id}>
-                    <h3>{trip.name}</h3>
-                    <p>price - {trip.price} mmk</p>
-                </li>
-            ))
+    <div className='container'>
+        {error && <p>{error}</p>}
+        { !error &&
+            <div className='flex-container'>
+            <h1>Ready to go?</h1>
+            
+            {loading && <p>Loading trips</p>}
+            <div>
+                <button onClick={()=>setUrl('http://localhost:3001/trips')}>All</button>
+                <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>Trip in Myanmar</button>
+            </div>
+            <ul className='trips-list'>
+                {
+                    trips && trips.map(trip=>(
+                        <li key={trip.id} className='trip'>
+                            <h3>{trip.name}</h3>
+                            <p>price - {trip.price} mmk</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
         }
-    </ul>
     </div>
   )
 }
